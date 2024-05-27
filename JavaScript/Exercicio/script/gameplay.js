91ab356a794d088491b463fe9ce019809c9ed864
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-05-27 11:00:27 -0300

    commit 3 / 27.05.2024

--- JavaScript/Exercicio/script/gameplay.js
+++ JavaScript/Exercicio/script/gameplay.js
@@ -1,24 +1,59 @@
 
 const parameter = new URLSearchParams(window.location.search);
 const dif = parameter.get('dif');
-
+// Variáveis do HTML
 const difField = document.getElementById('dif');
+const result = document.getElementById('result');
+const inputNumber = document.getElementById('inputNumber');
+const triesField = document.getElementById('tries');
+const tipsField = document.getElementById('tips');
+// Variáveis do Jogo
+let sortedNumber = 0;
+let tries = 4;
+let tips = 2;
+
+triesField.textContent = tries;
+tipsField.textContent = tips;
+
 
 switch(dif){
     case 'easy':
         difField.textContent = 'Dificuldade escolhida: Fácil';
+        sortedNumber = Math.floor(Math.random() * 30) + 1;
         break;
     case 'med':
         difField.textContent = 'Dificuldade escolhida: Médio';
+        sortedNumber = Math.floor(Math.random() * 60) + 1;
         break;
     case 'hard':
         difField.textContent = 'Dificuldade escolhida: Difícil';
+        sortedNumber = Math.floor(Math.random() * 90) + 1;
         break;
     default:
         difField.textContent = 'Não encontramos a dificuldade'
         break;
 }
 
+console.log(sortedNumber);
+
 function exit() {
     window.location.replace('./home.html');
+}
+
+
+function gameplay() {
+    let number = parseInt(inputNumber.value);
+
+    if ( number == sortedNumber ) {
+        result.textContent = 'Você acertou !!';
+        // Informa o usuário da vitória e logo em seguida manda para a tela inicial
+
+        return;
+    }
+    else {
+        result.textContent = 'Você errou !!';
+        tries = tries -1;
+
+        triesField.textContent = tries;
+    }
 }

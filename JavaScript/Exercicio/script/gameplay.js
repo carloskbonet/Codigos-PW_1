112a511e5d309a0bfaac34274a71a918e52ad56b
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-05-29 15:51:31 -0300

    commit 29.05.2024

--- JavaScript/Exercicio/script/gameplay.js
+++ JavaScript/Exercicio/script/gameplay.js
@@ -11,6 +11,10 @@ const tipsField = document.getElementById('tips');
 let sortedNumber = 0;
 let tries = 4;
 let tips = 2;
+// Variáveis para fazer a dica
+let maxSorted = 0;
+let sortedNumberTip = 0;
+let tipText = document.getElementById('tipText');
 
 triesField.textContent = tries;
 tipsField.textContent = tips;
@@ -20,14 +24,17 @@ switch(dif){
     case 'easy':
         difField.textContent = 'Dificuldade escolhida: Fácil';
         sortedNumber = Math.floor(Math.random() * 30) + 1;
+        maxSorted = 30;
         break;
     case 'med':
         difField.textContent = 'Dificuldade escolhida: Médio';
         sortedNumber = Math.floor(Math.random() * 60) + 1;
+        maxSorted = 60;
         break;
     case 'hard':
         difField.textContent = 'Dificuldade escolhida: Difícil';
         sortedNumber = Math.floor(Math.random() * 90) + 1;
+        maxSorted = 90;
         break;
     default:
         difField.textContent = 'Não encontramos a dificuldade'
@@ -63,14 +70,40 @@ function gameplay() {
 
             result.textContent = 'Buuuuuuh não foi dessa vez !!! Você tem ' + tries + ' tentativas restantes';
         }
-        else {
+        
+        if ( tries <= 0 ) {
             result.textContent = "GAME OVER";
 
             return exit();
         }
+    }
+}
 
-        // Dar uma dica sempre que o jogador errar (limitado ao número de dicas restantes)
-        // Dar uma dica ao pressionar um botão da interface
-        // Apertar um botão e solicitar um número para dar a dica
+
+function callTip() {
+    if ( tips <= 0 ) {
+        alert('Você não tem dicas restantes.');
+        
+        return;
+    }
+    else {
+        tips = tips - 1;
+        tipsField.textContent = tips;
+    }
+
+    sortedNumberTip = Math.floor(Math.random() * maxSorted) + 1;
+
+    if ( sortedNumberTip > sortedNumber ) {
+        // Falar que o número sorteado é MENOR que o número da dica
+        tipText.textContent = `O número sorteado é MENOR que ${sortedNumberTip}`
+    }
+    if ( sortedNumberTip < sortedNumber ) {
+        // Falar que o número sorteado é MAIOR que o número da dica
+        tipText.textContent = `O número sorteado é MAIOR que ${sortedNumberTip}`
+    }
+
+    if ( sortedNumberTip == sortedNumber ) {
+        // Escolher uma fala
+        tipText.textContent = `Tente TUDO menos ${sortedNumberTip}`
     }
 }

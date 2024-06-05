f9c99339deb60203243c2a76c551f38da772f66e
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-06-05 10:29:38 -0300

    Update Exemplo.sql

--- Database/SQL/Exemplo.sql
+++ Database/SQL/Exemplo.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Clientes (
+CREATE TABLE IF NOT EXISTS Clientes (
     id INTEGER PRIMARY KEY,
     nome VARCHAR(64) NOT NULL,
     idade INTEGER,
-    email VARCHAR(64) NOT NULL
+    email VARCHAR(64) UNIQUE NOT NULL
 );
 
 -- Para apagar a tabela, use o comando:
@@ -16,10 +16,27 @@ DROP TABLE Clientes;
 
 -- C / INSERT
 INSERT INTO Clientes (nome, idade, email) VALUES ('Roberto' , 27 , 'troberto.9165@gmail.com');
+INSERT INTO Clientes (nome, idade, email) VALUES ('Ana' , 21 , 'anap1.32@gmail.com');
+INSERT INTO Clientes (nome, idade, email) VALUES ('Mauro' , 42 , 'mmiz746@gmail.com');
 
 -- R / SELECT
 SELECT * FROM Clientes;
 SELECT nome,idade,email FROM Clientes;
+SELECT nome,idade,email,Biografia FROM Clientes WHERE email = 'anap1.32@gmail.com';
 
 -- U / Update
-UPDATE Clientes SET idade = 23 WHERE id = 2;
+UPDATE Clientes SET idade = 20;
+
+UPDATE Clientes SET idade = 27 WHERE id = 1;
+UPDATE Clientes SET idade = 22 WHERE id = 2;
+UPDATE Clientes SET idade = 42 WHERE id = 3;
+
+-- D / Delete
+DELETE FROM Clientes;
+
+DELETE FROM Clientes WHERE id = 1;
+
+-- Alterar Tabela
+ALTER TABLE Clientes ADD COLUMN Biografia VARCHAR(300);
+
+

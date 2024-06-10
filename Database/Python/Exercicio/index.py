bb0f56127645d7b34c3bf44d1138ef32fef9621a
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-06-10 11:25:56 -0300

    commit 2 / 10.06.2024

--- Database/Python/Exercicio/index.py
+++ Database/Python/Exercicio/index.py
@@ -17,6 +17,44 @@ while( True ):
 
     if ( inputMenu == 0 ):
         break;
+    
+    if ( inputMenu == 1 ):
+        try:
+            name = str('');
+            price = float(0);
+            quantity = int(0);
+        
+            print(f'\nDigite os dados solicitados para a criação do produto.');
+            name = str(input('Nome: '));
+            price = float(input('Preço: '));
+            quantity = int(input('Quantidade: '));
+        
+            response = produtos.create(name,price,quantity);
+        
+            print(f'{response['message']}');
+    
+        except:
+            print('Something went wrong');
+
+    # SELECT
+    if ( inputMenu == 2 ):
+        data = [];
+        data = produtos.select();
+
+        # Exibir dados corretamente
+        if ( data['code'] == 200 ):
+            
+            for product in data['data']:
+                print(f'\nNome: {product[0]} , Preço: {product[1]} R$ , ' + 
+                      f'Quantidade: {product[2]} un')
+
+        else:
+            # Erro de banco de dados
+            if ( data['code'] == 500 ):
+                print(f'\n{data['data']}');
+            else:
+                # Erro não previsto
+                print(f'\nUnknown Error');
 
 
-    input('Aperte ENTER para continuar.');
+    input('\nAperte ENTER para continuar.');
--- Database/Python/Exercicio/produtos.py
+++ Database/Python/Exercicio/produtos.py
@@ -3,6 +3,7 @@ import sqlite3;
 connection = sqlite3.connect('Store.db');
 cursor = connection.cursor();
 
+
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products (
         id INTEGER PRIMARY KEY,
@@ -12,15 +13,30 @@ cursor.execute('''
     );
 ''');
 
+#connection.close();
+
 def create(name:str , price:float , quantity:int):
-    cursor.execute('INSERT INTO Products (name,price,quantity) VALUES (?,?,?)', (name , price , quantity));
-    connection.commit();
+    try:
+        cursor.execute('INSERT INTO Products (name,price,quantity) VALUES (?,?,?)', (name , price , quantity));
+        connection.commit();
+    
+        return {'code' : 201 , 'message' : 'Product Created'};
+    except:
+        return {'code': 500 , 'message' : 'Internal Error'};
 
 def findBy():
     pass;
 
 def select():
-    pass;
+    try:
+        products = [];
+
+        cursor.execute('SELECT name,price,quantity FROM Products');
+        products = cursor.fetchall();
+        return { 'code': 200 , 'data': products};
+    
+    except:
+        return { 'code': 500 , 'data': 'Internal Error'};
 
 def update():
     pass;

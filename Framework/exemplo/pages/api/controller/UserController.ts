b1d84f87550eb2c5833f8fa123d4beccc176b167
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-06-26 11:28:51 -0300

    commit 26.06.2024

---
+++ Framework/exemplo/pages/api/controller/UserController.ts
@@ -0,0 +1,21 @@
+import { createUserModel } from "../model/user";
+
+export async function createUser(_username:string , _password:string , _confirmPassword:string , _cpf:string , _name = "") {
+    // Realizar verificações em atributos Únicos das tabelas.
+    // Verificar se os valores já estão cadastrados no banco de dados
+    try {
+        if( _password != _confirmPassword ) {
+            return { message: 'Passwords dont match' };
+        }
+
+        //Verificar username e cpf
+
+        const response = await createUserModel( _name , _username , _password , _cpf );
+
+        return response;
+
+    }
+    catch(err) {
+        return { message: 'Something went wrong' };
+    }
+}
---
+++ Framework/exemplo/pages/user/login.tsx
@@ -0,0 +1,11 @@
+
+
+export default function loginPage() {
+
+    return (
+        <main>
+            
+
+        </main>
+    );
+}

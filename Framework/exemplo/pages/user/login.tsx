85bd04a2f3b8d72e58c2b86f52182dae66ed997a
Sabo <57690741+carloskbonet@users.noreply.github.com>
2024-07-24 14:03:10 -0300

    commit 1 / 24.07.2024

--- Framework/exemplo/pages/user/login.tsx
+++ Framework/exemplo/pages/user/login.tsx
@@ -1,10 +1,22 @@
-
+import styles from '@/styles/login.module.css';
+import Head from 'next/head';
 
 export default function loginPage() {
 
     return (
-        <main>
-            
+        <main className='flex min-h-screen flex-col'>
+            <Head>
+                <title>Login</title>
+            </Head>
+
+            <form className={styles.container}>
+
+                <input type="text" placeholder='Nome de usuário' />
+                <input type="password" placeholder='Senha' />
+
+                <input type="submit" value="Login" />
+
+            </form>
 
         </main>
     );
